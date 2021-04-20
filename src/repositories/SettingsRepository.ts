63e2a99ffe8cacd1d5ebb82dc4148b51663ea99f
Andre Araujo <andreearaujo98@gmail.com>
2021-04-20 15:20:59 -0300

    first commit

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,9 @@
+import { Setting } from "../entities/Setting"
+import { EntityRepository, Repository } from "typeorm"
+
+@EntityRepository(Setting)
+class SettingsRepository extends Repository<Setting> {
+
+}
+
+export { SettingsRepository }
